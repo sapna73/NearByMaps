39d96c68c1e13b6c360af0c7c085e1b45f08ae08
sapna73 <sg.sapna73@gmail.com>
2022-01-10 18:25:51 +0530

    first commit

---
+++ app/src/main/java/com/example/mymaps/models/OpeningHours.kt
@@ -0,0 +1,8 @@
+package com.example.mymaps.models
+
+import com.google.gson.annotations.SerializedName
+
+data class OpeningHours(
+    @SerializedName("open_now")
+    var openNow: Boolean
+)
---
+++ app/src/main/java/com/example/mymaps/models/PlusCode.kt
@@ -0,0 +1,10 @@
+package com.example.mymaps.models
+
+import com.google.gson.annotations.SerializedName
+
+data class PlusCode(
+    @SerializedName("compound_code")
+    var compoundCode: String,
+    @SerializedName("global_code")
+    var globalCode: String
+)
